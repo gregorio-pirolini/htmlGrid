21a48a823485ad85f4da2682a54849064e7b6972
gregorio-pirolini <gregorioxx77@yahoo.de>
2022-09-29 08:53:22 +0100

    first draft

---
+++ js/script.js
@@ -0,0 +1,557 @@
+console.log("hello!");
+const videos = [
+  {
+    src: "quyXS4a0JGQ",
+    title: "11B X 1371",
+    res: 169,
+    text: `...`,
+  },
+  {
+    src: "AqovWZvsDcQ",
+    title: "spiral",
+    res: 169,
+    text: `...`,
+  },
+
+  {
+    src: "SRnmZFyCycs",
+    title: "Hiroshima （広島)",
+    res: 43,
+    text: `Yeah, Yeah! 
+音を初めて聞いた時
+それが人生変えたのね
+それは頭に忍び込んで
+とってもダーティな気分よ
+
+    Yeah, Yeah! 
+    帽子の中に入りたい
+    頭にかぶって!! 本気なの
+    カップに入れて飲みほしてよ
+    あんたの中に入りたい
+
+    Yeah, Yeah! 
+    ツバを吐くのが好きなのね
+    濡れてきちゃうわそれを見ると
+    あたしを頭にかぶってちょうだい
+    あんたの刺激になりたいの`,
+  },
+  {
+    src: "pD95huFiGd0",
+    title: "杏里　キャッツアイ",
+    res: 169,
+    text: `都会(まち)はきらめく
+    passion fruit
+    ウインクしてる everynight
+    グラスの中の passion beat
+    一口だけでfall in love
+    甘いメロディ 風にのれば今夜
+    秘密めいた扉が どこかで開くよ
+    見つめるCat's Eye
+    magic play is dancing
+    緑色に光る
+    妖しく Cat's Eye
+    magic play is dancing
+    月明かり浴びて
+    we get you...you...you...you...
+    mysterious girl
+    Bellを鳴らせば passion time
+    終わりを告げる every night
+    青いドレスの sexy girl
+    口づけしては 幻
+    古いピアノ 風にのれば夜明け
+    愛を映す
+    ミラーが どこかで割れるよ
+    魅かれて Cat's Eye
+    magic play is dancing
+    緑色に光る
+    迷って Cat's Eye
+    magic play is dancing
+    月明かり浴びて
+    we get you...you...you...you...
+    mysterious girl
+    見つめるCat's Eye
+    magic play is dancing
+    緑色に光る
+    妖しく Cat's Eye
+    magic play is dancing
+    月明かり浴びて
+    we get you...you...you...you...`,
+  },
+
+  {
+    src: "dMFtM7abCDI",
+    title: "The Prodigy - 'Girls'",
+    res: 169,
+    text: `Imagine how it would be
+    To be at the top making cash money
+    Go on tour all around the world
+    Tell stories about all the young girls
+    
+    Imagine how it would be
+    To be at the top making cash money
+    Go on tour all around the world
+    Tell stories about all the young girls
+    
+    Imagine how it would be
+    To be at the top making cash money
+    
+    Girls
+    
+    Travel fast
+    Have a blast
+    
+    Girls - girls - round the world
+    
+    Imagine how it would be
+    To be at the top making cash money
+    Go on tour all around the world
+    Tell stories about all the young girls`,
+  },
+  {
+    src: "GAbgqfwt9oM",
+    title: "Babes in Toyland - Ripe",
+    res: 43,
+    text: `I know I know I know shut up
+    Killer clean been a racecar on a skyway down
+    I've been shown these wrong ways
+    I now own
+    I now own all these wrong ways I've been shown
+    I know I know I know shut up
+    All the rapists keep asking me for cigarettes
+    I just try to grab hold of a solid piece of pavement and pray that
+    Yeah!
+    I have two legs
+    Leeann she has one
+    I know I know!
+    Shut up
+    My own notions go a choking my throat
+    All bad thoughts float around in a fury luring me into one big abyss of hit and miss
+    I empty all of this
+    Johnny walkeround my ninety pound braincloud of how?`,
+  },
+  {
+    src: "gjI0KYl9gWs",
+    title: "Pussy Riot - Путин научит тебя любить Родину",
+    res: 169,
+    text: `50 миллиардов и радужный Лучик,
+    Роднина и Кабаева факелы вручат.
+    В лагерях подчиняться и плакать научат.
+    Салют начальничкам, привет, Дуче!
+     
+    Сочи блокирован — Олимп под надзором.
+    Спецсредства, оружие, толпы ментов.
+    ФСБ — аргумент, МВД — аргумент,
+    По России-1 — аплодисменты.
+     
+    Путин научит тебя любить Родину.
+     
+    Весна в России бывает внезапной,
+    Привет мессии в качестве залпа.
+    Авроры, прокурор настроен по-хамски,
+    Ему нужен отпор, а не красивые глазки.
+     
+    Вольер для протеста, водка, матрешка,
+    Тюрьма для Болотной, наливка, икорка,
+    В петле конституция, на зоне Витишко,
+    Стабильность, пайка, забор, вышка.
+     
+    Путин научит тебя любить Родину.
+     
+    Для Дождя перекрыт эфир.
+    Гей-прайд отправлен в сортир.
+    Двухочковый туалет — приоритет.
+    Приговор России, режим общий, шесть лет.
+     
+    Путин научит тебя любить Родину.
+     
+    Родину
+    Родину
+    Родину`,
+  },
+];
+const asideContent = [
+  {
+    pix: "alienAbduction.jpg",
+    caption: "alienAbduction",
+    text: "Alien abduction narratives sometimes allege contact with reptilian creatures. One of the earliest reports was that of Ashland, Nebraska police officer Herbert Schirmer, who under hypnosis recalled being taken aboard a UFO in 1967 by humanoid beings with a slightly reptilian appearance, who wore a 'winged serpent' emblem on the left side of their chests. Skeptics consider his claims to be a hoax",
+    title: "Alien abduction",
+  },
+  {
+    pix: "reptilian.jpg",
+    caption: "reptilians humans comparaisons",
+    text: "Reptilians – also called reptoids, archons, reptiloids, saurians, or draconians  – are supposed reptilian humanoids, which play a prominent role in fantasy, science fiction, ufology, and conspiracy theories. The idea of reptilians was popularised by David Icke, a conspiracy theorist who claims shapeshifting reptilian aliens control Earth by taking on human form and gaining political power to manipulate human societies. Icke has stated on multiple occasions that many world leaders are, or are possessed by, so-called reptilians.",
+    title: "Alien abduction",
+  },
+  {
+    pix: "ufo.jpg",
+    caption: "Fermi-Paradoxon",
+    text: `Das Fermi-Paradoxon ist eine Bezeichnung für einen Gedankengang des Physikers Enrico Fermi aus dem Jahr 1950. Fermi ging davon aus, dass es extraterrestrische Intelligenz gibt, die technisch hochentwickelte Zivilisationen über Millionen von Jahren aufrechterhalten kann. In dieser Zeitspanne sollte es mittels interstellarer Raumfahrt möglich sein, die gesamte Galaxie zu kolonisieren – und der Wahrscheinlichkeit nach sollte dies bereits geschehen sein. Dass dennoch die Suche nach den Spuren von außerirdischem Leben bisher erfolglos blieb, erschien ihm paradox und als Hinweis darauf, entweder die Annahmen oder die Beobachtungen zu hinterfragen.`,
+    title: "Alien abduction",
+  },
+];
+
+const pixGallerie = [
+  {
+    pix: "ot1.jpg",
+    name: "name",
+    title: "title",
+    alt: "alt text",
+  },
+  {
+    pix: "ot1.jpg",
+    name: "name",
+    title: "title",
+    alt: "alt text",
+  },
+  {
+    pix: "ot1.jpg",
+    name: "name",
+    title: "title",
+    alt: "alt text",
+  },
+  {
+    pix: "ot2.jpg",
+    name: "name",
+    title: "title",
+    alt: "alt text",
+  },
+  {
+    pix: "ot3.webp",
+    name: "name",
+    title: "title",
+    alt: "alt text",
+  },
+  {
+    pix: "ot4.jpg",
+    name: "name",
+    title: "title",
+    alt: "alt text",
+  },
+  {
+    pix: "ot5.jpg",
+    name: "name",
+    title: "title",
+    alt: "alt text",
+  },
+  {
+    pix: "ot6.jpg",
+    name: "name",
+    title: "title",
+    alt: "alt text",
+  },
+  {
+    pix: "ot7.jpg",
+    name: "name",
+    title: "title",
+    alt: "alt text",
+  },
+  {
+    pix: "ot8.jpg",
+    name: "name",
+    title: "title",
+    alt: "alt text",
+  },
+  {
+    pix: "lizzard.jpg",
+    name: "name",
+    title: "title",
+    alt: "alt text",
+  },
+  { pix: "red1920.jpg", name: "name", title: "title", alt: "alt text" },
+];
+
+const components = [
+  {
+    pix: ["pix1", "pix2", "pix3"],
+    name: "name",
+    anzDLeitungen: 0,
+    anschliessbGeräte: "anschliessbGeräte",
+    datentransferrate: "datentransferrate", //in Mbit/s
+    GeräteerkennungHotpNeustart: "Geräteerkennung Hot pNeustart",
+    kabel: "kabel", //in meter,
+    ado: "ado",
+    spannung: "spannung", //in V
+    strom: "strom", //in mA
+  },
+  {
+    pix: "pix", // [""],
+    name: "name",
+    anzDLeitungen: 0,
+    anschliessbGeräte: "anschliessbGeräte",
+    datentransferrate: "", //in Mbit/s
+    GeräteerkennungHotpNeustart: "",
+    kabel: "", //in meter,
+    ado: "ado",
+    spannung: " ", //in V
+    strom: "", //in mA
+  },
+  {
+    pix: "pix", // [""],
+    name: "name",
+    anzDLeitungen: 0,
+    anschliessbGeräte: "anschliessbGeräte",
+    datentransferrate: "", //in Mbit/s
+    GeräteerkennungHotpNeustart: "",
+    kabel: "", //in meter,
+    ado: "ado",
+    spannung: " ", //in V
+    strom: "", //in mA
+  },
+];
+
+// for each in object Table
+// let myTable = document.getElementById("myTable");
+// components.forEach((element) => {
+//   let row = myTable.insertRow();
+//   for (const [key, value] of Object.entries(element)) {
+//     console.log(`${key}: ${value}`);
+//     let cell = row.insertCell();
+//     cell.innerHTML = value;
+//   }
+// });
+
+//buid components kupferschmittList
+
+//!Place images
+//! ██╗███╗   ███╗ █████╗  ██████╗ ███████╗███████╗
+//! ██║████╗ ████║██╔══██╗██╔════╝ ██╔════╝██╔════╝
+//! ██║██╔████╔██║███████║██║  ███╗█████╗  ███████╗
+//! ██║██║╚██╔╝██║██╔══██║██║   ██║██╔══╝  ╚════██║
+//! ██║██║ ╚═╝ ██║██║  ██║╚██████╔╝███████╗███████║
+//! ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚══════╝
+pixGallerie.forEach((element) => {});
+//!Place texts
+//! __________________________  ______________ _________
+//! \__    ___/\_   _____/\   \/  /\__    ___//   _____/
+//!   |    |    |    __)_  \     /   |    |   \_____  \
+//!   |    |    |        \ /     \   |    |   /        \
+//!   |____|   /_______  //___/\  \  |____|  /_______  /
+//!                    \/       \_/                  \/
+//texts.forEach((element) => {});
+//!Place SIDEBAR
+//!
+
+//!  SSSSS  IIIII DDDDD   EEEEEEE BBBBB     AAA   RRRRRR
+//! SS       III  DD  DD  EE      BB   B   AAAAA  RR   RR
+//!  SSSSS   III  DD   DD EEEEE   BBBBBB  AA   AA RRRRRR
+//!      SS  III  DD   DD EE      BB   BB AAAAAAA RR  RR
+//!  SSSSS  IIIII DDDDDD  EEEEEEE BBBBBB  AA   AA RR   RR
+asideContent.forEach((element) => {
+  let grid_container_section = document.createElement("div");
+  grid_container_section.className = "grid_container_section";
+
+  let grid_container_section_item = document.createElement("div");
+  grid_container_section_item.className = "grid_container_section_item";
+
+  let title = element.title;
+  let caption = element.caption;
+  let text = element.text;
+  let pix = element.text;
+
+  grid_container_section_item.innerHTML = "title";
+  grid_container_section_item.appendChild = "pix";
+  grid_container_section_item.appendChild = "caption";
+  grid_container_section.appendChild("title");
+}); // pix  caption  text  title
+
+//!Place videos
+//! ╦  ╦┬┌┬┐┌─┐┌─┐┌─┐
+//! ╚╗╔╝│ ││├┤ │ │└─┐
+//!  ╚╝ ┴─┴┘└─┘└─┘└─┘
+let $i = 0;
+videos.forEach((element) => {
+  let grid_containerVideos = document.createElement("div");
+  grid_containerVideos.className = "grid_containerVideos";
+
+  let videoFigure = document.createElement("div");
+  videoFigure.className = "videoFigure video" + element.res + " videoItem1";
+
+  let clickMe = document.createElement("div");
+  clickMe.className = "clickMe";
+  clickMe.style.backgroundImage =
+    "url(https://img.youtube.com/vi/" + element.src + "/mqdefault.jpg)";
+
+  clickMe.dataset.src = element.src;
+  clickMe.dataset.number = $i;
+  clickMe.title = "click to  play";
+  let img = document.createElement("img");
+  img.src = "img/youtube.png";
+
+  clickMe.appendChild(img);
+  let playerVideo = document.createElement("div");
+  playerVideo.id = "player" + $i;
+
+  let videoItem2 = document.createElement("div");
+  videoItem2.className = "videoItem2";
+  let title = document.createElement("p");
+  title.innerText = element.title;
+  let text = document.createElement("p");
+  text.innerText = element.text;
+  videoItem2.appendChild(title);
+  videoItem2.appendChild(text);
+  videoFigure.appendChild(clickMe);
+  videoFigure.appendChild(playerVideo);
+
+  grid_containerVideos.appendChild(videoFigure);
+  grid_containerVideos.appendChild(videoItem2);
+
+  let myVideos = document.getElementById("myVideos");
+  myVideos.appendChild(grid_containerVideos);
+  $i++;
+});
+
+//!                                                   _
+//!  components                                      | |
+//!   ___ ___  _ __ ___  _ __   ___  _ __   ___ _ __ | |_ ___
+//!  / __/ _ \| '_ ` _ \| '_ \ / _ \| '_ \ / _ \ '_ \| __/ __|
+//! | (_| (_) | | | | | | |_) | (_) | | | |  __/ | | | |_\__ \
+//!  \___\___/|_| |_| |_| .__/ \___/|_| |_|\___|_| |_|\__|___/
+//!                     | |
+//!                     |_|
+components.forEach((element) => {
+  let section = document.createElement("section");
+  let gridContainer = document.createElement("div");
+  gridContainer.className = "grid-container";
+  for (const [key, value] of Object.entries(element)) {
+    let gridItem = document.createElement("div");
+    let klassName;
+    key == "pix" ? (klassName = "grid-item pix") : (klassName = "grid-item");
+    gridItem.className = klassName;
+    gridItem.innerHTML = value;
+    gridContainer.appendChild(gridItem);
+  }
+  section.appendChild(gridContainer);
+  // let article = document.querySelectorAll("article");
+  let myArticle = document.getElementById("myArticle");
+
+  myArticle.appendChild(section);
+});
+
+const grid_container_nav_item = document.getElementsByClassName(
+  "grid_container_nav_item"
+);
+const item2 = document.getElementsByClassName("item2");
+
+let updateSmallMenu = (e) => {
+  let smallMenuNameP = document.querySelector("#smallMenuName p");
+
+  smallMenuNameP.innerText = e.target.text;
+};
+
+//! functions
+//!     ____                 __  _
+//!    / __/_  ______  _____/ /_(_)___  ____  _____
+//!   / /_/ / / / __ \/ ___/ __/ / __ \/ __ \/ ___/
+//!  / __/ /_/ / / / / /__/ /_/ / /_/ / / / (__  )
+//! /_/  \__,_/_/ /_/\___/\__/_/\____/_/ /_/____/
+
+let toogleSmallMenu = (state) => {
+  console.log("toogleSmallMenu " + state);
+  Object.keys(grid_container_nav_item).forEach((key) => {
+    if (key > 1) {
+      grid_container_nav_item[key].style.display = state;
+    }
+  });
+};
+
+let toogleSmallMenuCheck = () => {
+  console.log("i have been clicked");
+
+  grid_container_nav_item[2].style.display == "none"
+    ? toogleSmallMenu("block")
+    : toogleSmallMenu("none");
+
+  console.log(grid_container_nav_item[2].style.display);
+};
+
+//! events                     __
+//!   _______  __ ____   _____/  |_  ______
+//! _/ __ \  \/ // __ \ /    \   __\/  ___/
+//! \  ___/\   /\  ___/|   |  \  |  \___ \
+//!  \___  >\_/  \___  >___|  /__| /____  >
+//!      \/          \/     \/          \/
+
+const smallMenu = document.getElementById("smallMenu");
+smallMenu.addEventListener("click", toogleSmallMenuCheck);
+Object.keys(item2).forEach((key) => {
+  item2[key].addEventListener("click", updateSmallMenu);
+});
+
+const clickMe = document.getElementsByClassName("clickMe");
+
+Object.keys(clickMe).forEach((key) => {
+  clickMe[key].addEventListener("click", (event) => {
+    onYouTubeIframeAPIReady(event);
+  });
+});
+
+// youtube
+//!  Yb  dP  dP"Yb  88   88 888888 88   88 88""Yb 888888
+//!   YbdP  dP   Yb 88   88   88   88   88 88__dP 88__
+//!    8P   Yb   dP Y8   8P   88   Y8   8P 88""Yb 88""
+//!   dP     YbodP  `YbodP'   88   `YbodP' 88oodP 888888
+
+// $("body").on("click", ".clickMe", function () {
+//   console.log(`will load video:`);
+//   console.log($(this).attr("data-src"));
+//   var dataId = $(this).attr("data-src");
+//   var dataNumber = $(this).attr("data-number");
+
+//   onYouTubeIframeAPIReady(dataId, dataNumber);
+// });
+
+let player;
+
+function onYouTubeIframeAPIReady(event) {
+  console.log("onYouTubeIframeAPIReady!!!");
+  if (typeof event == "undefined") {
+    console.log("why doe it load before click??");
+    return;
+  }
+  console.log("onYouTubeIframeAPIRead");
+  let dataId = event.currentTarget.getAttribute("data-src");
+  let dataNumber = event.currentTarget.getAttribute("data-number");
+  let ids = event.currentTarget.id;
+  let playerToogle = document.getElementById("player" + dataNumber);
+  playerToogle.style.display = "block";
+  console.log(ids + ", " + dataNumber + ", " + dataId);
+
+  player = new YT.Player("player" + dataNumber, {
+    height: "360",
+    width: "640",
+    videoId: dataId,
+    playerVars: {
+      autoplay: 1,
+      controls: 1,
+      rel: 0,
+      fs: 0,
+      theme: "light",
+      color: "white",
+      mute: 0,
+      allow: "autoplay",
+    },
+    events: {
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange,
+    },
+  });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    // setTimeout(stopVideo, 6000);
+    done = true;
+  }
+}
+function stopVideo() {
+  player.stopVideo();
+}
